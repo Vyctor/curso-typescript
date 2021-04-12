550beb95e7a741260034247b9ac88c4b95aa3faa
Vyctor <dev.vyctor@gmail.com>
2021-04-11 22:25:29 -0300

    type void

---
+++ src/A0003-type-void/index.ts
@@ -0,0 +1,14 @@
+function semRetorno(...args: string[]): void {
+  console.log(args.join(' '));
+}
+const pessoa = {
+  nome: 'Luiz',
+  sobrenome: 'Otávio',
+  exibirNome(): void {
+    console.log(`${this.nome} ${this.sobrenome}`);
+  },
+};
+
+pessoa.exibirNome();
+
+export { pessoa };
