75d651550325506c0e13dfe2e37cfad62c1538d5
Vyctor <dev.vyctor@gmail.com>
2021-05-27 13:23:59 -0300

    type tuple

---
+++ src/A0006-type-tuple/index.ts
@@ -0,0 +1,15 @@
+const dadosCliente: readonly [number, string] = [1, 'Vyctor'];
+const dadosCliente2: [number, string, string] = [2, '', ''];
+const dadosCliente3: [number, string, string?] = [3, ''];
+const dadosCliente4: [number, string, ...Array<number>] = [
+  1,
+  '',
+  1,
+  2,
+  3,
+  4,
+  5,
+  6,
+  7,
+  8,
+];
